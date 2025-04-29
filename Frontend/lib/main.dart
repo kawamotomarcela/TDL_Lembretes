7def5fab92035661ba67b802276b474efb429bdd
kawamotomarcela <kawamotomarcela@gmail.com>
2025-04-29 18:51:05 -0300

    atividade3-Configura o TaskProvider no main.dart usando MultiProvider

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
-
 import 'routes/app_routes.dart';
 import 'providers/task_provider.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting('pt_BR', null);
-
+void main() {
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => TaskProvider()),
-        // Você pode adicionar outros Providers aqui no futuro
       ],
       child: const MyApp(),
     ),
@@ -35,6 +29,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 
 
